ce8d797297db37cf6ceeeb84301700c9548aa0f7
wipad <75433182+wipad@users.noreply.github.com>
2021-11-04 10:02:12 +0700

    Update ManagerPage.js

--- src/views/ManagerPage.js
+++ src/views/ManagerPage.js
@@ -1,5 +1,12 @@
 import * as React from 'react';
 import { useLocation } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
+function App() {
+    return <Button variant="contained">Hello World</Button>;
+  }
+  ReactDOM.render(<App />, document.querySelector('#app'));
+
 
 export default function ManagerPage(props) {
     let location = useLocation();
